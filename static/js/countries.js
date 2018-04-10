b54358568e1e2f382f5bea81b89c1775aee6db38
JonathanQuang <jquang@stuy.edu>
2018-04-10 13:59:54 -0400

    finished color country function

--- static/js/countries.js
+++ static/js/countries.js
@@ -2,18 +2,22 @@ console.log('js loaded');
 svg = document.getElementById("worldMap");
 pathList = document.getElementById("pathList");
 
-var colorCountry = function(countryName){
+var colorCountry = function(countryName,theColor){
 	countryPath = "null"
 
 	var i;
 
-	console.log(pathList.childNodes[1].getAttribute("title"));	
+	countryPath = "null"	
 
-	for (i=1;i < 351; i++){
-		//if (pathList.childNodes[i].getAttribute("title")==countryName){
-		//	countryPath=pathList.childNodes[i];
-		//}
-		console.log(pathList.childNodes[i].getAttribute("title");
+	for (i=1;i < 351; i+=2){
+		pathElement = pathList.childNodes[i];
+		title = pathElement.getAttribute("title");
+		if (title == countryName){
+			countryPath = pathElement;
+		}
 	}	
+
+	countryPath.style.fill = theColor;
+	return(countryPath);
 	
 }
