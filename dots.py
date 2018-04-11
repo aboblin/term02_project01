8f828c77a20923d85199ffe571c4b08ee931c84a
Manahal Tabassum <mtabassum@stuy.edu>
2018-04-10 20:33:50 -0400

    worked on dots

--- dots.py
+++ dots.py
@@ -1,6 +1,7 @@
 import csv
 
-year = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+data = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+dots = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 
 def getData():
     with open('stats.csv') as csvfile:
@@ -9,9 +10,27 @@ def getData():
             #print (row)
             #print (row[0])
             #print (row[1])
-            for i in range(24):
-                year[i].append(row[i])
-                #print year
-    print year
+            print len(data)
+            for i in range(len(data)):
+                #print data[i]
+                print row[i]
+                data[i].append(row[i])
+    #print data
+
+def makeDots():
+    for i in range(24):
+        #print data[i]
+        for x in data[i]:
+            try:
+                y = int(x)
+                #print (int(x)/10000)
+                if (y%10000 == 0):
+                    dots[i].append(y/10000)
+                else:
+                    dots[i].append(y/10000 + 1)
+            except:
+                pass
+    print dots
 
 getData()
+makeDots()
