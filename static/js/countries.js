f0b04f03c7a805872fdcb7084e21105eb80d3737
Manahal Tabassum <mtabassum@stuy.edu>
2018-04-13 14:15:01 -0400

    started working again

--- static/js/countries.js
+++ static/js/countries.js
@@ -1,6 +1,8 @@
 console.log('js loaded');
-svg = document.getElementById("worldMap");
-pathList = document.getElementById("pathList");
+var svg = document.getElementById("worldMap");
+var pathList = document.getElementById("pathList");
+var data = {};
+var index = 0;
 
 var colorCountry = function(countryName,theColor){
     var countryPath = "null"
@@ -26,10 +28,35 @@ var getData = function(){
 	type: "GET",
 	data: {},
 	success: function(d){
+	    data = d;
 	    console.log(d);
 	}
     });
     console.log("hello");
 };
+
+var getIndex = function(year){
+    if (year <= 1839){
+	index = 1;
+    }
+    else if (year <= 1849){
+	index = 
+    }
+#does not work
+#subtract 1830
+#mod 10
+#add 1
+}
+
+var makeDots = function(year){
+
+}
+
+
+
+
+
+
+
 
 //getData();
