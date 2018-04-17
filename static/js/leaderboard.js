4e3d6bc0af9abae63007dcb646291539556f80c4
JonathanQuang <jquang@stuy.edu>
2018-04-17 14:02:52 -0400

    eaderboard ajax request fixed

--- static/js/leaderboard.js
+++ static/js/leaderboard.js
@@ -5,23 +5,44 @@ var getTopFiveList = function(){
 		url:"/getTopFiveList",
 		type:"GET",
 		data:{},
-		success: function(d){
-			console.log(d);
-			return(d);
-		}
+		success: handleData
 	});
 	console.log("lboard");
 }
 
-leaderBoardList = getTopFiveList();
+leaderBoardList = "null"
+
+var handleData = function(data){
+	leaderBoardList = data;
+	console.log(leaderBoardList);
+}
+
+getTopFiveList()
+console.log("leaderBoardLoaded");
 console.log(leaderBoardList)
 
 slider = document.getElementById("year");
 
+var inputYearToIndex = function(yearNum){
+	if (yearNum < 2010){
+		workingYear = yearNum - 1820;
+		workingYear = Math.floor(workingYear/10);
+		return workingYear
+	}
+	else {
+		workingYear = yearNum - 2010;
+		return workingYear + 18
+	}
+	console.log('hi')
+}
+
+
 var yearUpdate = function(){
 	
 	var update = function(){
-		console.log(slider.innerHTML);
+		yearIndex = inputYearToIndex(parseInt(slider.innerHTML));
+		console.log(leaderBoardList['results'][yearIndex][0]);
+		
 	}
 	setInterval(update,250);
 }
