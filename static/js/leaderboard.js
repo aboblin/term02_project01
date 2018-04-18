bba52356429008e4f259a12ec4376c1383673b56
JonathanQuang <jquang@stuy.edu>
2018-04-18 14:02:04 -0400

    leaderboard now shos on svg

--- static/js/leaderboard.js
+++ static/js/leaderboard.js
@@ -1,4 +1,12 @@
 console.log("leaderboard loaded")
+lBoardElemArray = []
+for (var i=1;i<=5;i++){
+	lBoardElem=document.getElementById("lBoard"+i.toString());
+	lBoardElemArray.push(lBoardElem);
+}
+
+
+
 
 var getTopFiveList = function(){
 	$.ajax({
@@ -40,8 +48,12 @@ var inputYearToIndex = function(yearNum){
 var yearUpdate = function(){
 	
 	var update = function(){
-		yearIndex = inputYearToIndex(parseInt(slider.innerHTML));
+		var yearIndex = inputYearToIndex(parseInt(slider.innerHTML));
 		console.log(leaderBoardList['results'][yearIndex][0]);
+		for (var i=1;i<=5;i++){
+			var subInString = leaderBoardList['results'][yearIndex][i-1];
+			lBoardElemArray[i-1].innerHTML=i.toString()+". " + subInString;
+		}
 		
 	}
 	setInterval(update,250);
